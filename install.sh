a55709c21e0d90d1f0108363dbca8fc1d38df96d
PastLeo <chgu82837@gmail.com>
2022-05-01 22:59:24 +0800

    Update install.sh
    Github: The unauthenticated git protocol on port 9418 is no longer supported.

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@
 # ```
 
 if [[ ! -f $HOME/.homesick/repos/homeshick/homeshick.sh ]]; then
-  git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+  git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 fi
 
 source $HOME/.homesick/repos/homeshick/homeshick.sh
